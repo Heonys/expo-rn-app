033c0a7aaf47d6cadc1a67024985c7523a72b7ce
Jiheon Kim <siwmua0@naver.com>
2023-12-02 04:47:02 +0900

    initalize

--- redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import todoReducer from "./slice/todoSlice";
-
-export const store = configureStore({
-  reducer: {
-    todo: todoReducer,
-  },
-});
